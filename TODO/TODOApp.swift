5a785ce1825fa612e496b14328f85136c8f0afa9
Yvysanna <yvette.violetta@web.de>
2023-09-22 12:59:53 +0200

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Yvette Schröder on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
